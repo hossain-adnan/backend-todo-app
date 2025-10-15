780a7fa48fbeb1406bb09f9e43b104123b8634bc
hossain-adnan <63507205+hossain-adnan@users.noreply.github.com>
2025-10-15 11:29:56 +0600

    Implement login process

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -27,11 +27,32 @@ router.post('/register', (req, res) => {
 
     } catch (error) {
         console.log(error.message);
-        res.status(503).send(error.message)
+        res.status(503).send(error.message);
     }
 })
 
 //Login
+router.post('/login', (req, res) => {
+    //check the username and password with the database
+    const { username, password } = req.body;
+    try {
+        const getUser = db.prepare(`SELECT * FROM users where username = ?`);
+        const user = getUser.get(username);
+
+        if (!user) return res.status(404).send({ message: "User not found" });
+
+        const passwordIsValid = bcrypt.compareSync(password, user.password);
+
+        if (!passwordIsValid) return res.status(404).send({ message: "Password invalid" });
+
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '24h' });
+
+        res.send({token});
+    } catch (error) {
+        console.log(error.message);
+        res.status(503).send(error.message);
+    }
+})
 
 
 export default router;
